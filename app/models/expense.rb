425bad4304feba1b8e777a648aa950950c608e70
Taher <ttaherhagui@gmail.com>
2022-03-14 16:30:37 +0100

    fix linter.

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,12 +1,12 @@
 class Expense < ApplicationRecord
   belongs_to :user
   has_many :operation_types
-  has_many categories, through: :operation_types 
+  has_many categories, through: :operation_types
   validates :name, presence: true
   validates :amount, presence: true
   validates :amount, numericality: { greater_than: 0 }
-  
-  def create (categories_list)
+
+  def create(categories_list)
     categories_list.each do |cat|
       new_operation = OperationType.new(expense: self, category: cat)
       new_operation.save!
