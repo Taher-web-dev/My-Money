efad4be06ee7bf05adc7a4d1521bfca62e82d8a5
Taher <ttaherhagui@gmail.com>
2022-03-16 15:26:24 +0100

    fix linter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :success
   before_action :authenticate_user!
   before_action :update_allowed_parameters, if: :devise_controller?
-  
+
   protected
-  
+
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
