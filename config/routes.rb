592534b5c7352a06f4b3deb7adbe9c85455f77aa
Taher <ttaherhagui@gmail.com>
2022-03-16 10:17:09 +0100

    add some details to user authentification.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
+  get "users", to: "home#index"
+  
+  root "home#index"
 end
